b888c0b99af9e3def6778be26142af50618eed31
yuanshenMASTER <162892798+yuanshenMASTER@users.noreply.github.com>
2024-09-13 16:28:09 +0800

    SG90.h

--- SG90.h
+++ SG90.h
@@ -3,10 +3,7 @@
 #include "stm32f10x.h"
 
 void SG90_Init(void); 
-//void Leme_lelf(void);
-//void Leme_right(void);
-//void Leme_mid(void);
-//void Servo_SetAngle(float Angle);
+
 void PWM_SetCompare2(uint16_t Compare);
 	
 #endif
